0f1925625176a6277d68339098ce9f3916c322f0
Rob Antonisse <robantonisse@gmail.com>
2022-07-09 11:20:13 +0200

    Start StePPer V2.01

---
+++ StePPer_V2.01/StePPer_V2.01.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		StePPer_V2.ino
+ Created:	7/9/2022 11:19:42 AM
+ Author:	gebruiker
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
